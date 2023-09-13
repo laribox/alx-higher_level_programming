862dd7709dd6403ab39974f0eb98e7cd0d39586b
laribox <laribox2014@gmail.com>
2023-09-13 23:30:18 +0000

    task 3 - 6

---
+++ 0x04-python-more_data_structures/6-print_sorted_dictionary.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+def print_sorted_dictionary(a_dictionary):
+    list_o = list(a_dictionary.keys())
+    list_o.sort()
+    for i in list_ord:
+        print("{}: {}".format(i, a_dictionary.get(i)))
