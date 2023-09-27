3ed63f65ecd4514d350f48134cb7ca59ad4e80a3
laribox <laribox2014@gmail.com>
2023-09-27 02:45:18 +0100

    task 1

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
+
 """definition for class square"""
 
 
--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,16 +1,14 @@
 #!/usr/bin/python3
-"""Square class definition"""
+
+"""Define a class Square."""
 
 
 class Square:
-    """Represents a square
-    Attributes:
-        __size (int): size of a side of the square
-    """
+    """Represent a square."""
     def __init__(self, size):
-        """Initializes a square
-        Args:
-            size (int): size of a side of the square
-        Returns: None
+        """init the class intance
+
+        args:
+            size (int): the size of the square
         """
         self.__size = size
