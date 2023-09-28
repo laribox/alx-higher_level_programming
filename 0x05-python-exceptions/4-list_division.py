5cfbed70164cdd6d8ffceb8421ed4caa6b7ee959
laribox <laribox2014@gmail.com>
2023-09-28 01:55:42 +0100

    fix

--- 0x05-python-exceptions/4-list_division.py
+++ 0x05-python-exceptions/4-list_division.py
@@ -1,8 +1,4 @@
 #!/usr/bin/python3
-
-"""function that divides element by element 2 lists."""
-
-
 def list_division(my_list_1, my_list_2, list_length):
     list = []
     for i in range(list_length):
