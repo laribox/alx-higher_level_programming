e5af2d2332f4840a75da662fc7d8bedb230ad1c6
laribox <laribox2014@gmail.com>
2023-10-09 23:20:10 +0100

    task 2

---
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+""" checks if object is an instance of the specified class"""
+
+
+def is_same_class(obj, a_class):
+    """return true if obj is of type a_class"""
+    if type(obj) is a_class:
+        return True
+    else:
+        return False
