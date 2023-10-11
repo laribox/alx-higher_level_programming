04662c889caa06ed0236db27cd8c28e76825c262
laribox <laribox2014@gmail.com>
2023-10-11 01:06:50 +0100

    fix

--- 0x0B-python-input_output/6-load_from_json_file.py
+++ 0x0B-python-input_output/6-load_from_json_file.py
@@ -2,13 +2,13 @@
 
 """ creates an Object from a JSON file"""
 import json
-
+"""imports json module"""
 
 
 def load_from_json_file(filename):
     """
     creates an Object
-    from a JSON file
+    from a “JSON file”
     """
     with open(filename, mode="r", encoding="utf-8") as file:
         return json.load(file)
