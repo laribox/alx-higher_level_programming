d9a73b4a4eb0c678b02b753570f201f52b60302f
laribox <laribox2014@gmail.com>
2023-10-11 01:10:21 +0100

    task 9 10

---
+++ 0x0B-python-input_output/10-student.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+
+"""class Student"""
+
+
+class Student:
+    """student class"""
+
+    def __init__(self, first_name, last_name, age):
+        """
+        an instance of class
+        with three public attributes:
+        first_name, last_name and age
+        """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """
+        retrieves a dictionary representation of attrs if its a list else
+        """
+        if isinstance(attrs, list):
+            d = {}
+            for attr in attrs:
+                if hasattr(self, attr):
+                    d[attr] = getattr(self, attr)
+            return d
+        return self.__dict__
