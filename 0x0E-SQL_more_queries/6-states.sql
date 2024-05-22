ff45b5f9b2992ed4ec2954c2288e406b8479fb97
larini abdellah <laribox2014@gmail.com>
2024-05-22 16:30:06 +0100

    Update 6-states.sql

--- 0x0E-SQL_more_queries/6-states.sql
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -2,6 +2,7 @@
 --  in the database hbtn_0d_usa) on your MySQL server
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa 
 CREATE TABLE IF NOT EXISTS hbtn_0d_usa.states (
-    id INT UNIQUE NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    PRIMARY KEY(id),
+    id INT UNIQUE NOT NULL AUTO_INCREMENT,
     name VARCHAR(256) NOT NULL 
 );
