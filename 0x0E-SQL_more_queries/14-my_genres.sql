890ed003110a7d616a996b095492199a1bacc1f4
larini abdellah <laribox2014@gmail.com>
2024-05-23 02:49:47 +0100

    Update 14-my_genres.sql

--- 0x0E-SQL_more_queries/14-my_genres.sql
+++ 0x0E-SQL_more_queries/14-my_genres.sql
@@ -1,4 +1,4 @@
--- a script that uses the hbtn_0d_tvshows database to lists all genres of the show Dexter
+-- script that uses the hbtn_0d_tvshows database to lists all genres of the show Dexter
 SELECT tv_genres.name
 FROM tv_genres
 INNER JOIN tv_show_genres 
