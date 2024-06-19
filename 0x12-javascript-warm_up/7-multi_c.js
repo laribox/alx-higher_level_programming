8ae24ed1b376cf85b0c25a9dae6882abfa7e031f
laribox <laribox2014@gmail.com>
2024-06-19 05:48:58 -0700

    task 7 answer

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -3,9 +3,8 @@ const x = Number(process.argv[2]);
 
 if (isNaN(x)) {
   console.log('Missing number of occurrences');
-  return;
-}
-
-for (let index = 0; index < x; index++) {
-  console.log('C is fun');  
+} else {
+  for (let index = 0; index < x; index++) {
+    console.log('C is fun');
+  }
 }
