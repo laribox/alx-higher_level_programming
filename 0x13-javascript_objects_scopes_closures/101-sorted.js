9acc837730b31f4043e117c2aff226353fff05fa
laribox <laribox2014@gmail.com>
2024-06-19 19:26:47 -0700

    task 12

---
+++ 0x13-javascript_objects_scopes_closures/101-sorted.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+const { dict } = require('./101-data');
+const result = {};
+
+for (const userId in dict) {
+  const occurrence = dict[userId];
+
+  if (!result[occurrence]) {
+    result[occurrence] = [];
+  }
+
+  result[occurrence].push(userId);
+}
