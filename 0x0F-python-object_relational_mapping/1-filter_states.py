3d278da36caee2b1c94db974aef3d94c7ca9d73d
laribox <laribox2014@gmail.com>
2024-06-23 15:11:50 -0700

    task 1 awnser

---
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+ 
+"""  lists all states from the database hbtn_0e_0_usa """
+import MySQLdb
+import sys
+ 
+ 
+if __name__ == "__main__":
+     user = sys.argv[1]
+     password = sys.argv[2]
+     db_name = sys.argv[3]
+     db = MySQLdb.connect(host="localhost", user=user,
+                          passwd=password, db=db_name, port=3306)
+     cur = db.cursor()
+     cur.execute("SELECT * FROM states WHERE name like 'N%' ORDER BY id ASC")
+     rows = cur.fetchall()
+     for row in rows:
+         print(row)
+     cur.close()
+     db.close()
