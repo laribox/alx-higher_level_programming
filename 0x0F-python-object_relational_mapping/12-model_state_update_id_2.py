2108f2c51b223a942721b4e3d87bcb3d2961dc98
laribox <laribox2014@gmail.com>
2024-06-23 18:16:45 -0700

    task 12 awnser

--- 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
+++ 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
@@ -14,6 +14,7 @@ if __name__ == "__main__":
     Base.metadata.create_all(engine)
     Session = sessionmaker(bind=engine)
     session = Session()
-    state = session.query(State).filter(State.id==2).first()
+    state = session.query(State).filter(
+        State.id == 2).first()
     state.name = "New Mexico"
     session.commit()
