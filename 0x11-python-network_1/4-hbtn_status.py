187bd8b9c95ef34fe58d6508f2f232558e52c1c4
laribox <laribox2014@gmail.com>
2024-07-05 08:01:14 -0700

    task 3 answer

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -9,9 +9,9 @@ the body of the response (decoded in utf-8).
 import requests
 
 if __name__ == "__main__":
-  r = requests.get('https://alx-intranet.hbtn.io/status')
-  print(
-    "Body response:\n"
-    "\t- type: {}\n"
-    "\t- content: {}".format(type(r.text), r.text)
-  )
+    r = requests.get('https://alx-intranet.hbtn.io/status')
+    print(
+        "Body response:\n"
+        "\t- type: {}\n"
+        "\t- content: {}".format(type(r.text), r.text)
+    )
