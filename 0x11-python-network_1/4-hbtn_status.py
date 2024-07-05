bb80118128538acd03818790eb217ab4c9bda559
laribox <laribox2014@gmail.com>
2024-07-05 07:54:19 -0700

    task 3 answer

---
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""
+Python script that takes in a URL,
+sends a request to the URL and displays
+the body of the response (decoded in utf-8).
+"""
+
+
+import requests
+import sys
+
+if __name__ == "__main__":
+  r = requests.get('https://alx-intranet.hbtn.io/status')
+  print(
+    "Body response:\n"
+    "\t- type: {}\n"
+    "\t- content: {}".format(type(r.text), r.text)
+  )
