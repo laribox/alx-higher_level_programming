f7a62405bff01222f2bde053d1cd6db1d90bd3b9
laribox <laribox2014@gmail.com>
2024-07-05 06:47:48 -0700

    init and task 0

---
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""Defines a peak-finding algorithm."""
+
+
+import urllib.request
+
+with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') as response:
+    res = response.read()
+
+print(
+    "Body response:\n\t\
+    - type: {}\n\t\
+    - content: {}\n\t\
+    - utf8 content: {}"
+    .format(type(res), res, res.decode("utf-8")))
